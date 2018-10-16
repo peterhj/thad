4993c8aa3ca02c4c5964b3472862ac11df388077
Peter Jin <peterhaijin@gmail.com>
2018-10-16 10:21:59 -0700

    Add some common initializers.

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-`thad` is a collection of simple autodiff wrappers and utils for pytorch.
+`thad` is a collection of simple autodiff wrappers and utils for pytorch v0.3.1.
--- thad/utils.py
+++ thad/utils.py
@@ -23,9 +23,33 @@ class ParamsBuilder(object):
       param_init_fns.append(init_fn)
     return param_vars, param_init_fns
 
+def const_init(data, dtype=th.FloatTensor):
+  def init_fn():
+    return th.from_numpy(data).type(dtype)
+  return init_fn
+
 def zeros_init(dim, dtype=th.FloatTensor):
   def init_fn():
     return th.zeros(*dim).type(dtype)
+  return init_fn
+
+def uniform_init(dim, lo, hi, dtype=th.FloatTensor):
+  def init_fn():
+    x = np.random.uniform(low=lo, high=hi, size=dim).astype(np.float32)
+    return th.from_numpy(x).type(dtype)
+  return init_fn
+
+def normal_init(dim, mean, std, dtype=th.FloatTensor):
+  def init_fn():
+    x = np.random.normal(mean, std, size=dim).astype(np.float32)
+    return th.from_numpy(x).type(dtype)
+  return init_fn
+
+def orth_normal_init(dim, orth_axis, dtype=th.FloatTensor):
+  def init_fn():
+    x = np.random.normal(0.0, 1.0, size=dim).astype(np.float32)
+    x /= np.linalg.norm(x, axis=orth_axis, keepdims=True)
+    return th.from_numpy(x).type(dtype)
   return init_fn
 
 def normal_linear_init(kernel_dim, mean, std, dtype=th.FloatTensor):
